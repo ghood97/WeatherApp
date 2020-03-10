1972771650dd558c1efb8ee3a67fa76bc947cb34
ghood97 <griffinhood@verizon.net>
2020-03-09 21:08:55 -0400

    Write CurrentCondition and Location models

---
+++ WeatherApp/Models/CurrentCondition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class TempSystem
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public TempSystem Metric { get; set; }
+        public TempSystem Imperial { get; set; }
+    }
+
+    public class CurrentCondition
+    {
+        public DateTime LocalObservationDateTime { get; set; }
+        public int EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public object PrecipitationType { get; set; }
+        public bool IsDayTime { get; set; }
+        public Temperature Temperature { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+
+}
