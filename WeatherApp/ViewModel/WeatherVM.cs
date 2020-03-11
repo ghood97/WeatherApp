69225187f2722eeb98ce70083b6e2934687db08f
ghood97 <griffinhood@verizon.net>
2020-03-11 00:19:10 -0400

    Implement and bind the ICommand interface to the search button

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -4,12 +4,54 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.ViewModel.Commands;
+using WeatherApp.ViewModel.Helpers;
 
 namespace WeatherApp.ViewModel
 {
     public class WeatherVM : INotifyPropertyChanged
     {
         private string query;
+        private CurrentCondition currentCondition;
+        private Location selectedCity;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new Location
+                {
+                    LocalizedName = "Boston"
+                };
+                CurrentCondition = new CurrentCondition
+                {
+                    WeatherText = "Sunny",
+                    Temperature = new Temperature
+                    {
+                        Imperial = new TempSystem
+                        {
+                            Value = 65
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public CurrentCondition CurrentCondition
+        {
+            get { return currentCondition; }
+            set
+            {
+                currentCondition = value;
+                OnPropertyChanged("CurrentCondition");
+            }
+        }
 
         public string Query
         {
@@ -21,8 +63,20 @@ namespace WeatherApp.ViewModel
             }
         }
 
+        public Location SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public async void MakeQuery()
+        {
+            var locations = await AccuWeatherHelper.GetLocations(Query);
+        }
 
         private void OnPropertyChanged(string propertyName)
         {
